ae9b81aa74dc45310ebe86aca46a30892e9a0ca6
Jeonseungwoo1 <149984505+Jeonseungwoo1@users.noreply.github.com>
2024-03-04 17:18:43 +0900

    first commit

---
+++ tensor.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+class MyDenseLayer(tf.keras.layers.Layer) :
+    def __init__(self, input_dim, output_dim):
+        super(MyDenseLayer, self).__init__()
+
+
+        self.W = self.add_weight([input_dim, output_dim])
+        self.b = self.add_weight([1, output_dim])
+
+        def call(self, inputs):
+            z = tf.matmul(inputs, self.W) + self.b
+
+
+            output = tf.math.sigmin(z)
+            return output
---
+++ test.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+from icecream import ic
+
+ic(tf.__version__)
